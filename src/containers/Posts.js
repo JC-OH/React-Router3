497ddc0675c7700caf10d6a5d247b393ec38aad1
Jicheol Oh <jicheol@gmail.com>
2019-06-18 14:27:21 -0700

    Posts 컴포넌트 수정하기

--- src/containers/Posts.js
+++ src/containers/Posts.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import BigText from '../components/BigText';
 
-const Posts = () => {
+
+// 저희는 Post 컴포넌트가 Posts 컴포넌트의 내부에서 보여지게 할 계획입니다.
+// 마치, App 컴포넌트 내부에서 헤더를 보여주고 그 하단에 Home, About, Posts 컴포넌트를 보여준 것 처럼 말이죠.
+
+// 그러려면, Posts 컴포넌트에서 Post 컴포넌트가 보여질 부분에 children 을 렌더링 하면 됩니다.
+const Posts = ({children}) => {
     return (
         <div>
             <BigText>포스트</BigText>
+            {children}
         </div>
     );
 };
